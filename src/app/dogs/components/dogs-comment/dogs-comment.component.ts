c22258f6a62784b6680f33d263a243e3c2ef6a22
Mohamed Zidani <mzidani@sqli.com>
2021-03-15 16:56:03 +0100

    g c

---
+++ src/app/dogs/components/dogs-comment/dogs-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dogs-comment',
+  templateUrl: './dogs-comment.component.html',
+  styleUrls: ['./dogs-comment.component.css']
+})
+export class DogsCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
